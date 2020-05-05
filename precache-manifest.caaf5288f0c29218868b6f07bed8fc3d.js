eb276bb72ebfac8d0f648ff354fc0cf216590b31
Imcber <imcber1@gmail.com>
2020-05-04 23:31:41 -0500

    Updates

---
+++ precache-manifest.caaf5288f0c29218868b6f07bed8fc3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e76d0237b34897fc979824619cd917e",
+    "url": "/saber/index.html"
+  },
+  {
+    "revision": "c5123337da3548fdb3dd",
+    "url": "/saber/static/css/main.eb21461f.chunk.css"
+  },
+  {
+    "revision": "36c8c686999f888eef30",
+    "url": "/saber/static/js/2.706dd2a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saber/static/js/2.706dd2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5123337da3548fdb3dd",
+    "url": "/saber/static/js/main.3e22766e.chunk.js"
+  },
+  {
+    "revision": "eec13a1a3a0d10c1e6bf",
+    "url": "/saber/static/js/runtime-main.c631fd5a.js"
+  }
+]);
